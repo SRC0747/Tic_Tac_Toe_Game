4d0ae4cf434acf1f2c0c0bfcd04463a9ab75bb22
SRC0747 <srcsrk1998@gmail.com>
2021-08-10 21:31:47 +0530

    added: Player choose X or O and decide if play the game or not.

--- src/com/Tic_Tac_Toe_Game_Workshop/TicTacToeGame.java
+++ src/com/Tic_Tac_Toe_Game_Workshop/TicTacToeGame.java
@@ -8,10 +8,33 @@ public class TicTacToeGame {
         char[] board=new char[10];
         System.out.println("The TicTacToe board for a player to start fresh is created.");
         createBoard(scan,board);
+        System.out.println("Player choose X or O and decide if play the game or not.");
+        playerComputer();
     }
     public static void createBoard(Scanner scan, char[] board)
     {
         for(int index=1; index<board.length; index++)
             board[index]= scan.next().charAt(0);
+    }
+    public static void playerComputer()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("You have two choices to check if you can play the TicTacToe game or not ?");
+        System.out.println("X:Cannot play the Game.");
+        System.out.println("O: Can Play the game.");
+        System.out.println("Enter your choice X or O.");
+        char ch = scan.next().charAt(0);
+        switch(ch)
+        {
+            case 'X':
+                System.out.println("Sorry, You cannot Play the Game.");
+                break;
+            case 'O':
+                System.out.println("Congratulations,You can Play the Game.");
+                break;
+            default:
+                System.out.println("Wrong input.");
+                break;
+        }
     }
 }
