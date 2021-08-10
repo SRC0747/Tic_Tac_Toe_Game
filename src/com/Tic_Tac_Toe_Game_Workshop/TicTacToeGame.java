f0f199acc02a0b78a94a21587abae8859fcb82ac
SRC0747 <srcsrk1998@gmail.com>
2021-08-10 20:46:20 +0530

    added: The TicTacToe board for a player to start fresh is created.

--- src/com/Tic_Tac_Toe_Game_Workshop/TicTacToeGame.java
+++ src/com/Tic_Tac_Toe_Game_Workshop/TicTacToeGame.java
@@ -1,8 +1,17 @@
 package com.Tic_Tac_Toe_Game_Workshop;
-
+import java.util.Scanner;
 public class TicTacToeGame {
     public static void main(String[] args)
     {
         System.out.println("Welcome to Tic-Tac-Toe Game.");
+        Scanner scan = new Scanner(System.in);
+        char[] board=new char[10];
+        System.out.println("The TicTacToe board for a player to start fresh is created.");
+        createBoard(scan,board);
+    }
+    public static void createBoard(Scanner scan, char[] board)
+    {
+        for(int index=1; index<board.length; index++)
+            board[index]= scan.next().charAt(0);
     }
 }
