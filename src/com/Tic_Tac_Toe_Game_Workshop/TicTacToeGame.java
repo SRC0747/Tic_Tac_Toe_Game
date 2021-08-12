6ffb21ac56034f4e9f5f5137f2d99f9f62326b69
SRC0747 <srcsrk1998@gmail.com>
2021-08-12 20:49:55 +0530

    added: Creating and showing the Current board.

--- src/com/Tic_Tac_Toe_Game_Workshop/TicTacToeGame.java
+++ src/com/Tic_Tac_Toe_Game_Workshop/TicTacToeGame.java
@@ -37,4 +37,14 @@ public class TicTacToeGame {
                 break;
         }
     }
+    public static int showBoard()
+    {
+        System.out.println("|---|---|---|");
+        System.out.println(" | "+board[1]+" | "+board[2]+" | "+board[3]+" | ");
+        System.out.println("|-----------|");
+        System.out.println(" | "+board[4]+" | "+board[5]+" | "+board[6]+" | ");
+        System.out.println("|-----------|");
+        System.out.println(" | "+board[7]+" | "+board[8]+" | "+board[9]+" | ");
+        System.out.println("|---|---|---|");
+    }
 }
